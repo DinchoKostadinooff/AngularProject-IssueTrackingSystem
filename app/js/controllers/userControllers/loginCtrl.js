f537e136333ec415cd3a8253855a06ba8746f407
DinchoKostadinooff <dincho1997@abv.bg>
2016-04-28 18:12:47 +0300

    Revert "IssueTrackingSystem"
    This reverts commit 448d802c4454ff726cbaa6ae2254294477818907.

--- app/js/controllers/userControllers/loginCtrl.js
+++
@@ -1,8 +0,0 @@
-app.controller('LoginController', ['$location', 'AuthenticationService', function($location, AuthenticationService) {
-    var ctrl = this;
-    ctrl.login = login;
-
-    function login() {
-        AuthenticationService.Login(ctrl.username, ctrl.password);
-    }
-}]);
--- app/js/controllers/userControllers/logoutCtrl.js
+++
@@ -1,9 +0,0 @@
-app.controller('LogoutController', ['AuthenticationService', function(AuthenticationService) {
-    var controller = this;
-    controller.logout = logout;
-    controller.logout();
-
-    function logout() {
-        AuthenticationService.Logout();
-    }
-}]);
